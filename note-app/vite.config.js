ff910b5b8f9fbdc948147b1e8287c0954e23c5c0
moteki-0035-git <motekiruna.9684@gmail.com>
2025-02-04 04:19:47 +0000

    本番環境をビルドした

--- note-app/vite.config.js
+++ note-app/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Myapp/note-app/", // デプロイ先のパス
+  base: "./", // デプロイ先のパス
   server: {
     host: true, // 外部からのアクセスを許可
     port: 3000, // 任意のポート番号
