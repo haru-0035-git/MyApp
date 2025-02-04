ef5d74541d83721560a32c921b50e756349d3700
moteki-0035-git <motekiruna.9684@gmail.com>
2025-02-04 04:13:19 +0000

    aaa

--- note-app/vite.config.js
+++ note-app/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/Myapp/note-app/", // デプロイ先のパス
+  server: {
+    host: true, // 外部からのアクセスを許可
+    port: 3000, // 任意のポート番号
+  },
+});
